444f51b886825e98f64ea78cf0a00bab8d8756ec
Adam Macioszek <amacioszek4@gmail.com>
2018-08-09 13:34:44 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,10 +4,11 @@
 # in Classes to organize the information.
 
 import pyodbc
-from Parse import parseActual, display, parseHistorical, compareDay, compareBlocks, compareTrips
-from Classes import Day, Block, Trip, Segment, Stop
-
-
+import sys
+from Parse import parseActual, display, parseHistorical, compareDay, compareBlocks, compareTrips, adjustOnboards
+from Classes import Day, Block, Trip, Segment, Stop, Deviations, dTrip
+from datetime import timedelta, date, datetime  #for date conversion 
+from Deviations import report
 
 connection = pyodbc.connect(
     r'DRIVER={ODBC Driver 11 for SQL Server};'
@@ -17,108 +18,78 @@ connection = pyodbc.connect(
     r'PWD=*L12c3by'
     )
 
+#This function issues two queries to the databases to pull information about the actual and the scheduled trips.
+#It pulls all the information from a series of columns and the information is parsed row by row using the parseActual
+#and the parseHHistorical function. After that the function calls compare on the actual day object and the scheduled 
+#day object to compare the segements. 
+
+def daterange(start_date, end_date):
+    for n in range(int ((end_date - start_date).days)):
+        yield start_date + timedelta(n)
+
+
 
 def createDays(date):
+   # my_date_string = "2017/12/09"
+   # date1="2017-12-09"
+   # d = datetime.strptime(my_date_string, "%Y/%m/%d")
+    print(date)
     cursor = connection.cursor()
-    print(str(date))
+    
+
     selectHistoricalInformation = ( " SELECT service_day,trip_seq, stop_seq, route, trip, direction, block,Pattern_Record_Id, trip_start, trip_end, iStop_Id, tStop_Id,iStop_descr,tStop_descr, segment_feet"
                                 " FROM dbo.vHistorical_Stop_Schedule "
-                                " WHERE service_day = '2017-01-25' "
+                                " WHERE service_day = ?"
                                 " ORDER BY trip_seq, stop_seq asc")
 
 
     selectActualInformation = ( " SELECT  Message_Type_Id, service_date, block, route, trip, dir, vmh_time, bus, Onboard, boards, alights, Stop_Id,Internet_Name "
                                 " FROM dbo.vActual_History"
-                                " WHERE service_date = '2017-01-25' "
-                                " ORDER BY vmh_time asc")
+                                " WHERE service_date =? "
+                                "ORDER BY vmh_time asc")
+   
 
-
-    cursor.execute(selectActualInformation)
+    cursor.execute(selectActualInformation, date)
     currentActualDay=Day(date)
     results = cursor.fetchone()
     while results:
         currentActualDay=parseActual(currentActualDay, results)
         results = cursor.fetchone()
-    #display(currentActualDay)
 
+    print(1)
 
-    cursor.execute(selectHistoricalInformation)
+    cursor.execute(selectHistoricalInformation,date)
     currentHistoricalDay=Day(date)
     results = cursor.fetchone()
     while results:
         currentHistoricalDay=parseHistorical(currentHistoricalDay, results)
         results = cursor.fetchone()
-    #display(currentHistoricalDay)
-    testUpload(currentActualDay,currentHistoricalDay,cursor)
-    #compareDay(currentActualDay,currentHistoricalDay)
+    print(2)
+    compareDay(currentActualDay,currentHistoricalDay)
+    adjustOnboards(currentActualDay)
+    writeToSegments(currentActualDay, currentHistoricalDay,cursor)
+    checkSequence(currentActualDay)
+    report(currentActualDay)
+    #for t in currentActualDay.deviations.tripsMissed:
+    #    print(t)
 
 
-def testUploadHistorical(currentHistoricalDay,cursor):
-    for b in currentHistoricalDay.blocks:
-        for t in b.trips:
-            for s in t.segments:
-                hblocknum=b.blockNumber
-                htripnum=t.tripNumber
-                hiStopNumber=s.segmentID[0].stopId
-                htStopNumber=s.segmentID[1].stopId
-                print('('+str(hiStopNumber)+' , '+str(htStopNumber) +')')
-                cursor.execute(" INSERT INTO dbo.seg_trips (block2, trip2 , histop1, htstop2)"
-                " VALUES(?,?,?,?)", hblocknum, htripnum, hiStopNumber, htStopNumber)
-                connection.commit()
 
-                
-def test(currentActualDay, currentHistoricalDay,cursor):
-    for hb in currentHistoricalDay.blocks:
-        b=currentActualDay.getBlock(hb.blockNumber)
-        if b==None:
-            print('Whoops')
-        else:
-            print(str(b.blockNumber)+', '+ str(hb.blockNumber))
-            for ht in hb.trips:
-                t=b.getTrip(ht.tripNumber)
-                if t==None:
-                    print(str(ht.tripNumber)+'; '+ 'could not find')
-                else:
-                    print(str(t.tripNumber)+'; '+ str(ht.tripNumber))
-                    for s in ht.stops:
-                        print(s.stopId)
-
-
-def testUpload(currentActualDay, currentHistoricalDay,cursor):
+def checkSequence(currentActualDay):
     for b in currentActualDay.blocks:
-        hb=currentHistoricalDay.getBlock(b.blockNumber)
         for t in b.trips:
-            if hb==None:
-                break
-            else:
-                ht=hb.getTrip(t.tripNumber)
-                #ind=0
-                if ht==None:
-                    break
+            ind=0
+            while ind < len(t.segments):
+                if ind==0 and t.segments[ind].segmentSeq != 1:
+                    print(str(b.blockNumber)+':'+str(t.tripNumber)+' case 1')
+                    ind=ind+1
                 else:
-                    #for s in t.segments:
-                    for s, hseg in zip(t.segments,ht.segments):
-                    #   if ind >= len(ht.segments):
-                    #        break
-                    #    else:
-                            ablocknum=b.blockNumber
-                            atripnum=t.tripNumber
-                            aiStopNumber=s.segmentID[0].stopId
-                            atStopNumber=s.segmentID[1].stopId
-                    #        hseg=ht.segments[ind]
-                            hblocknum=hb.blockNumber
-                            htripnum=ht.tripNumber
-                            hiStopNumber=hseg.segmentID[0].stopId
-                            htStopNumber=hseg.segmentID[1].stopId
-                        #    ind=ind+1
-                        #    print('('+str(hiStopNumber)+' , '+str(htStopNumber) +')')
-                            cursor.execute(" INSERT INTO dbo.seg_trips ( ablock, hblock, atrip, htrip, aistop, atstop, histop, htstop)"
-                            " VALUES(?,?,?,?,?,?,?,?)", ablocknum,hblocknum, atripnum,htripnum, aiStopNumber,  atStopNumber,hiStopNumber, htStopNumber)
-                            connection.commit()
+                    if ind>0 and t.segments[ind].segmentSeq != (t.segments[ind-1].segmentSeq + 1) :
+                        print(str(b.blockNumber)+' : '+str(t.tripNumber)+'case 2')
+                    ind=ind+1
 
-def writeToSegments():
+def writeToSegments(currentActualDay, currentHistoricalDay,cursor):
     for b in currentActualDay.blocks:
-        hb=currentHistoricalDay.getBlock(b.blockNumber)
         for t in b.trips:
             for s in t.segments:
                 day=currentActualDay.date
@@ -127,22 +98,43 @@ def writeToSegments():
                 route=t.route
                 tripNumber=t.tripNumber
                 direction=t.direction
-                iStopNumber=s.segmentID[0].stopId
+                iStopNumber=s.segmentID[0].stopID
                 iStopName=s.segmentID[0].stopName
-                iStopType=s.segmentID[0].messageId
-
-                tStopNumber=s.segmentID[1].stopId
+                iStopType=s.segmentID[0].messageTypeID
+                iStopSeen=s.segmentID[0].seen
+                tStopNumber=s.segmentID[1].stopID
                 tStopName=s.segmentID[1].stopName
-                tStopType=s.segmentID[1].messageId
-                numberboards=s.numberBoardsInitialStop
-                numberalights=s.numberAlightsTerminalStop
+                tStopType=s.segmentID[1].messageTypeID
+                tStopSeen=s.segmentID[1].seen
+                boards=s.segmentID[0].boards 
+                alights=s.segmentID[1].alights
                 onboard=s.onboard
-                stopseq=0
+                adjOnboard=s.adjustedOnboard
                 iStopTime=s.segmentID[0].stopTime
-                tStopTime=s.segmentID[0].stopTime
-                segDist=s.distance
-                cursor.execute(" INSERT INTO dbo.Segments (ServiceDate, Bus, Block, Route, Trip, Pattern, Direction, iStopID, iStopName,iStopType, tStopID,tStopName, tStopType, Boards, Alights,Onboard,StopSeq,StartTime,EndTime,SegmentFeet)"
-                " VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", day, bus, blockNumber,route, tripNumber,0,direction, iStopNumber,iStopName,iStopType, tStopNumber,tStopName,tStopType, numberboards, numberalights,onboard,stopseq,iStopTime,tStopTime, segDist )
+                tStopTime=s.segmentID[1].stopTime
+                segDistance=s.distance
+                pattern=t.pattern
+                segseq=s.segmentSeq
+                cursor.execute(" INSERT INTO dbo.Segments (ServiceDate, Bus, Block, Route, Trip, Pattern, Direction, iStopID, iStopName, iStopMessageID, iStopSeen, tStopID, tStopName, tStopMessageID, tStopSeen, Boards, Alights, Onboard,AdjustedOnboard , SegmentSequence, StartTime, EndTime, SegmentFeet)"
+                " VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", day, bus, blockNumber,route, tripNumber,pattern,direction, iStopNumber,iStopName,iStopType, iStopSeen, tStopNumber,tStopName,tStopType, tStopSeen, boards, alights ,onboard ,adjOnboard ,segseq,iStopTime,tStopTime,segDistance )
                 connection.commit()
-createDays('2017-01-25')
+
+
+y1 = int(sys.argv[1])
+m1 = int(sys.argv[2])
+d1 = int(sys.argv[3])
+
+y2 = int(sys.argv[4])
+m2 = int(sys.argv[5])
+d2 = int(sys.argv[6])
+
+start_date = date(y1, m1, d1)
+end_date = date(y2, m2, d2)
+for single_date in daterange(start_date, end_date):
+    day= single_date.strftime("%Y-%m-%d")
+    createDays(day)
+
+
 connection.close()
+
+
